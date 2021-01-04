f935449b35092a570a0aa88accf773b1279c9019
undnull <undnull@yandex.ru>
2021-01-05 00:09:47 +0500

    ia32: removed the unnecessary prefix in the config

--- arch/ia32/include/arch/address.h
+++ arch/ia32/include/arch/address.h
@@ -2,7 +2,7 @@
 #define __ARCH_ADDRESS_H__ 1
 #include <demos/config.h>
 
-#define V2P(va) ((va) - CONFIG_MEM_BASE_ADDR)
-#define P2V(pa) ((pa) + CONFIG_MEM_BASE_ADDR)
+#define V2P(va) ((va) - CONFIG_BASE_ADDR)
+#define P2V(pa) ((pa) + CONFIG_BASE_ADDR)
 
 #endif
