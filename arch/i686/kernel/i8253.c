e6e6bdceb7d32c4ac7d5bf55529fb0f7e39df379
undnull <undnull@yandex.ru>
2021-01-06 20:56:25 +0500

    i686: pit cleanup

--- arch/i686/kernel/i8253.c
+++ arch/i686/kernel/i8253.c
@@ -2,10 +2,6 @@
 #include <arch/interrupts.h>
 #include <arch/io.h>
 
-#include <arch/serial.h>
-#include <demos/string.h>
-#include <demos/sprintf.h>
-
 #define PIT_FREQ 1193182
 #define PIT_CH0 0x40
 #define PIT_CMD 0x43
@@ -16,13 +12,6 @@ static void pit_intr(struct interrupt_frame *frame)
 {
     unused_nw(frame);
     pit_ticks++;
-
-    if(pit_ticks % PIT_TPS == 0) {
-        char buffer[64] = { 0 };
-        memset(buffer, 0, sizeof(buffer));
-        snprintf(buffer, sizeof(buffer), "ds: 0x%08X\n", pit_ticks / PIT_TPS);
-        serial_write(0, buffer, strlen(buffer));
-    }
 }
 
 void init_pit(void)
@@ -30,7 +19,7 @@ void init_pit(void)
     set_interrupt_handler(0x20, pit_intr);
 
     uint16_t divisor = PIT_FREQ / PIT_TPS;
-    outb(PIT_CMD, 0x34); // ch0, rate generator, 16-bit binary
+    outb(PIT_CMD, 0x34); // CH0, rate generator, 16-bit binary
     outb(PIT_CH0, (divisor & 0x00FF));
     outb(PIT_CH0, (divisor & 0xFF00) >> 8);
 }
--- include/demos/sprintf.h
+++ include/demos/sprintf.h
@@ -5,8 +5,8 @@
 #include <demos/compiler.h>
 
 int sprintf(char *s, const char *fmt, ...) __printf(2, 3);
-int vsprintf(char *s, const char *fmt, va_list va);
+int vsprintf(char *s, const char *fmt, va_list ap);
 int snprintf(char *s, size_t n, const char *fmt, ...) __printf(3, 4);
-int vsnprintf(char *s, size_t n, const char *fmt, va_list va);
+int vsnprintf(char *s, size_t n, const char *fmt, va_list ap);
 
 #endif
