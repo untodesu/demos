0928f68aa074e7ef0809f9496fdedec549ff640d
undnull <41549137+undnull@users.noreply.github.com>
2021-07-16 16:19:29 +0500

    GDT works now

--- arch/x86_64/include/arch/segment.h
+++ arch/x86_64/include/arch/segment.h
@@ -14,36 +14,16 @@
 #if !defined(__ASSEMBLER__)
 #include <arch/stdint.h>
 
-struct gdt_entry {
-    uint16_t limit_0;
-    uint16_t base_0;
-    uint8_t base_1;
-    union {
-        uint8_t b;
-        struct {
-            uint8_t ac : 1;
-            uint8_t rw : 1;
-            uint8_t down : 1;
-            uint8_t exec : 1;
-            uint8_t user : 1;
-            uint8_t ring : 2;
-            uint8_t present : 1;
-        } s;
-    } access;
-    union {
-        uint8_t b;
-        struct {
-            uint8_t limit_1 : 4;
-            uint8_t reserved : 1;
-            uint8_t is_64bit : 1;
-            uint8_t is_32bit : 1;
-            uint8_t is_4K : 1;
-        } s;
-    } flags_limit;
-    uint8_t base_2;
-} __packed;
+#define GDT_READWRITE   (1 << 1)
+#define GDT_CONFORMING  (1 << 2)
+#define GDT_EXECUTABLE  (1 << 3)
+#define GDT_NONSYSTEM   (1 << 4)
+#define GDT_RING_0      (0 << 5)
+#define GDT_RING_3      (3 << 5)
 
-void set_gdt_entry(uint8_t id, const struct gdt_entry *entry);
+void set_gdt_entry_16(uint8_t id, uint32_t base, uint16_t limit, uint8_t flags);
+void set_gdt_entry_32(uint8_t id, uint32_t base, uint32_t limit, uint8_t flags);
+void set_gdt_entry_64(uint8_t id, uint8_t flags);
 void init_gdt(void);
 #endif
 
--- arch/x86_64/kernel/segment.c
+++ arch/x86_64/kernel/segment.c
@@ -1,118 +1,88 @@
 #include <arch/segment.h>
+#include <arch/stdint.h>
 #include <demos/cdefs.h>
 #include <string.h>
 
-struct gdt_ptr {
+#define GDT_FATTY   (1 << 3)
+#define GDT_32BIT   (1 << 2)
+#define GDT_64BIT   (1 << 1)
+#define GDT_PRESENT (1 << 7)
+
+struct gdt_entry {
+    uint16_t limit_0;
+    uint16_t base_0;
+    uint8_t base_1;
+    uint8_t flags_0;
+    uint8_t limit_1 : 4;
+    uint8_t flags_1 : 4;
+    uint8_t base_2;
+} __packed;
+
+struct gdt_pointer {
     uint16_t limit;
     uint64_t base;
 } __packed;
 
 static struct gdt_entry gdt[7] = { 0 };
-static struct gdt_ptr gdt_ptr = { 0 };
+static struct gdt_pointer gdt_ptr = { 0 };
 
-void set_gdt_entry(uint8_t id, const struct gdt_entry *entry)
+void set_gdt_entry_16(uint8_t id, uint32_t base, uint16_t limit, uint8_t flags)
 {
-    memcpy(gdt + id, entry, sizeof(struct gdt_entry));
+    struct gdt_entry entry = { 0 };
+    entry.limit_0 = limit;
+    entry.base_0 = base & 0xFFFF;
+    entry.base_1 = (base >> 16) & 0xFF;
+    entry.base_2 = (base >> 24) & 0xFF;
+    entry.flags_0 = flags | GDT_PRESENT;
+    memcpy(gdt + id, &entry, sizeof(entry));
+}
+
+void set_gdt_entry_32(uint8_t id, uint32_t base, uint32_t limit, uint8_t flags)
+{
+    struct gdt_entry entry = { 0 };
+    entry.limit_0 = limit;
+    entry.limit_1 = (limit >> 16) & 0x0F;
+    entry.base_0 = base & 0xFFFF;
+    entry.base_1 = (base >> 16) & 0xFF;
+    entry.base_2 = (base >> 24) & 0xFF;
+    entry.flags_0 = flags | GDT_PRESENT;
+    entry.flags_1 = GDT_FATTY | GDT_32BIT;
+    memcpy(gdt + id, &entry, sizeof(entry));
+}
+
+void set_gdt_entry_64(uint8_t id, uint8_t flags)
+{
+    struct gdt_entry entry = { 0 };
+    entry.flags_0 = flags | GDT_PRESENT;
+    entry.flags_1 = GDT_64BIT;
+    memcpy(gdt + id, &entry, sizeof(entry));
 }
 
 void init_gdt(void)
 {
-    memset(gdt, 0, sizeof(gdt));
+    set_gdt_entry_16(GDT_INDEX_CODE16, 0x00000000, 0xFFFF, GDT_READWRITE | GDT_NONSYSTEM | GDT_RING_0 | GDT_EXECUTABLE);
+    set_gdt_entry_16(GDT_INDEX_DATA16, 0x00000000, 0xFFFF, GDT_READWRITE | GDT_NONSYSTEM | GDT_RING_0);
+    set_gdt_entry_32(GDT_INDEX_CODE32, 0x00000000, 0xFFFFF, GDT_READWRITE | GDT_NONSYSTEM | GDT_RING_0 | GDT_EXECUTABLE);
+    set_gdt_entry_32(GDT_INDEX_DATA32, 0x00000000, 0xFFFFF, GDT_READWRITE | GDT_NONSYSTEM | GDT_RING_0);
+    set_gdt_entry_64(GDT_INDEX_CODE64, GDT_READWRITE | GDT_NONSYSTEM | GDT_RING_0 | GDT_EXECUTABLE);
+    set_gdt_entry_64(GDT_INDEX_DATA64, GDT_READWRITE | GDT_NONSYSTEM | GDT_RING_0);
 
-    struct gdt_entry entry = { 0 };
-
-    // 16-bit code, base: 0x00000000, limit: 0xFFFFF, 4K: true
-    entry.limit_0 = 0xFFFF;
-    entry.flags_limit.s.limit_1 = 0xF;
-    entry.base_0 = 0x0000;
-    entry.base_1 = 0x00;
-    entry.base_1 = 0x00;
-    entry.access.s.rw = 1;
-    entry.access.s.exec = 1;
-    entry.access.s.ring = 0;
-    entry.access.s.present = 1;
-    entry.flags_limit.s.is_4K = 1;
-    set_gdt_entry(GDT_INDEX_CODE16, &entry);
-
-    // 16-bit data, base: 0x00000000, limit: 0xFFFFF, 4K: true
-    entry.limit_0 = 0xFFFF;
-    entry.flags_limit.s.limit_1 = 0xF;
-    entry.base_0 = 0x0000;
-    entry.base_1 = 0x00;
-    entry.base_1 = 0x00;
-    entry.access.s.rw = 1;
-    entry.access.s.exec = 0;
-    entry.access.s.ring = 0;
-    entry.access.s.present = 1;
-    entry.flags_limit.s.is_4K = 1;
-    set_gdt_entry(GDT_INDEX_DATA16, &entry);
-
-    // 32-bit code, base: 0x00000000, limit: 0xFFFFF, 4K: true
-    entry.limit_0 = 0xFFFF;
-    entry.flags_limit.s.limit_1 = 0xF;
-    entry.base_0 = 0x0000;
-    entry.base_1 = 0x00;
-    entry.base_1 = 0x00;
-    entry.access.s.rw = 1;
-    entry.access.s.exec = 1;
-    entry.access.s.ring = 0;
-    entry.access.s.present = 1;
-    entry.flags_limit.s.is_32bit = 1;
-    entry.flags_limit.s.is_4K = 1;
-    set_gdt_entry(GDT_INDEX_CODE32, &entry);
-
-    // 32-bit data, base: 0x00000000, limit: 0xFFFFF, 4K: true
-    entry.limit_0 = 0xFFFF;
-    entry.flags_limit.s.limit_1 = 0xF;
-    entry.base_0 = 0x0000;
-    entry.base_1 = 0x00;
-    entry.base_1 = 0x00;
-    entry.access.s.rw = 1;
-    entry.access.s.exec = 0;
-    entry.access.s.ring = 0;
-    entry.access.s.present = 1;
-    entry.flags_limit.s.is_32bit = 1;
-    entry.flags_limit.s.is_4K = 1;
-    set_gdt_entry(GDT_INDEX_DATA32, &entry);
-
-    // 64-bit code, base: 0x00000000, limit: 0xFFFFF, 4K: true
-    entry.limit_0 = 0xFFFF;
-    entry.flags_limit.s.limit_1 = 0xF;
-    entry.base_0 = 0x0000;
-    entry.base_1 = 0x00;
-    entry.base_1 = 0x00;
-    entry.access.s.rw = 1;
-    entry.access.s.exec = 1;
-    entry.access.s.ring = 0;
-    entry.access.s.present = 1;
-    entry.flags_limit.s.is_64bit = 1;
-    entry.flags_limit.s.is_4K = 1;
-    set_gdt_entry(GDT_INDEX_CODE64, &entry);
-
-    // 64-bit data, base: 0x00000000, limit: 0xFFFFF, 4K: true
-    entry.limit_0 = 0xFFFF;
-    entry.flags_limit.s.limit_1 = 0xF;
-    entry.base_0 = 0x0000;
-    entry.base_1 = 0x00;
-    entry.base_1 = 0x00;
-    entry.access.s.rw = 1;
-    entry.access.s.exec = 0;
-    entry.access.s.ring = 0;
-    entry.access.s.present = 1;
-    entry.flags_limit.s.is_64bit = 1;
-    entry.flags_limit.s.is_4K = 1;
-    set_gdt_entry(GDT_INDEX_DATA64, &entry);
-
-    gdt_ptr.limit = (uint16_t)(sizeof(gdt));
+    gdt_ptr.limit = (uint16_t)(sizeof(gdt) - 1);
     gdt_ptr.base = (uint64_t)(gdt);
 
-    // This little piece of shit double (or tripple?) faults
+    asm volatile("lgdtq %0"::"m"(gdt_ptr));
     asm volatile(
-        "lgdtq %0\n"
-        "pushw %1\n"
-        "pushq $1f\n"
-        "retfq\n"
-        "1:\n"
-        ::"m"(gdt_ptr), "i"(GDT_SELECTOR(0, 0, GDT_INDEX_CODE64))
+        "mov %1, %%ax   \n"
+        "mov %%ax, %%ds \n"
+        "mov %%ax, %%es \n"
+        "mov %%ax, %%fs \n"
+        "mov %%ax, %%gs \n"
+        "mov %%ax, %%ss \n"
+        "pushq %0       \n"
+        "pushq $1f      \n"
+        "retfq          \n"
+        "1:             \n"
+        :
+        : "i"(GDT_SELECTOR(0, 0, GDT_INDEX_CODE64)), "i"(GDT_SELECTOR(0, 0, GDT_INDEX_DATA64))
     );
 }
