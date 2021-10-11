29b0fe0b84473d65e1d727a614361a3a746749e9
undnull <41549137+undnull@users.noreply.github.com>
2021-10-11 22:05:09 +0500

    i8259: handle spurious interrupts

--- arch/x86_64/drivers/i8259.c
+++ arch/x86_64/drivers/i8259.c
@@ -1,23 +1,35 @@
 #include <drivers/i8259.h>
-#include <sys/interrupts.h>
+#include <sys/kprintf.h>
 #include <sys/ports.h>
 
-#define I8259_PORT_1 0x20
-#define I8259_PORT_2 0xA0
+#define I8259_PORT_1        0x20
+#define I8259_PORT_2        0xA0
+#define I8259_CASCADE_IRQ   0x02
 
-static uint16_t mask = 0xFFFF & ~(1 << 2);
+static unsigned int mask = 0;
+
+static inline unsigned int read_isr(void)
+{
+    outb(I8259_PORT_1, 0x0B);
+    outb(I8259_PORT_2, 0x0B);
+    return (inb(I8259_PORT_2) << 8) | inb(I8259_PORT_1);
+}
 
 void init_i8259(void)
 {
+    /* Mask everything */
+    outb(I8259_PORT_1 + 1, 0xFF);
+    outb(I8259_PORT_2 + 1, 0xFF);
+
     /* ICW1 - start initializing */
-    outb(I8259_PORT_1 + 0, (1 << 4) | (1 << 0));
-    outb(I8259_PORT_2 + 0, (1 << 4) | (1 << 0));
+    outb(I8259_PORT_1, (1 << 4) | (1 << 0));
+    outb(I8259_PORT_2, (1 << 4) | (1 << 0));
 
     /* ICW2 - new vector offsets */
     outb(I8259_PORT_1 + 1, I8259_IRQ(0x00));
     outb(I8259_PORT_2 + 1, I8259_IRQ(0x08));
 
-    /* ICW3 - chain IRQ2 to the second chip */
+    /* ICW3 - cascade IRQ2 to the second chip */
     outb(I8259_PORT_1 + 1, (1 << 2));
     outb(I8259_PORT_2 + 1, (1 << 1));
     
@@ -25,23 +37,31 @@ void init_i8259(void)
     outb(I8259_PORT_1 + 1, (1 << 0));
     outb(I8259_PORT_2 + 1, (1 << 0));
 
-    /* Mask everything */
+    /* Mask everything except cascading IRQ */
+    mask = 0xFFFF & ~(1 << I8259_CASCADE_IRQ);
     outb(I8259_PORT_1 + 1, mask & 0xFF);
     outb(I8259_PORT_2 + 1, (mask >> 8) & 0xFF);
 }
 
 void i8259_mask(unsigned int irq, int set)
 {
-    uint16_t irq_bit = (1 << irq);
-    uint16_t port = (irq_bit & 0xF0) ? I8259_PORT_2 : I8259_PORT_1;
-    uint16_t shift = (irq_bit & 0xF0) ? 8 : 0;
-    mask = set ? (mask | irq_bit) : (mask & ~irq_bit);
+    unsigned int bit = (1 << irq);
+    unsigned int shift = (bit & 0xF0) ? 8 : 0;
+    uint16_t port = (bit & 0xF0) ? I8259_PORT_2 : I8259_PORT_1;
+    mask = set ? (mask | bit) : (mask & ~bit);
     outb(port + 1, (mask >> shift) & 0xFF);
 }
 
 void i8259_send_eoi(unsigned int irq)
 {
-    if(irq >= 0x08)
-        outb(I8259_PORT_2, 0x20);
-    outb(I8259_PORT_1, 0x20);
+    unsigned int bit = (1 << irq);
+    if(read_isr() & bit) {
+        if(bit & 0xF0)
+            outb(I8259_PORT_2, 0x20);
+        outb(I8259_PORT_1, 0x20);
+        return;
+    }
+
+    /* There's an imposter among us */
+    kprintf("i8259: warning: spurious IRQ %u\r\n", irq);
 }
