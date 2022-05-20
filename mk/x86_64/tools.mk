003742d90ae99e32cbcca3ea67d0c138dad7e82d
Kirill GPRB <41549137+undnull@users.noreply.github.com>
2022-05-21 00:36:07 +0500

    Sooooo.... My main PC runs Linux natively.
    And I've rebuilt the cross compiler.

--- mk/x86_64/tools.mk
+++ mk/x86_64/tools.mk
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: BSD-2-Clause
 # Created: Sat Apr 23 2022 21:13:49
 
-AS		:= x86_64-unknown-elf-gcc
-AR		:= x86_64-unknown-elf-ar
-CC		:= x86_64-unknown-elf-gcc
-CPP		:= x86_64-unknown-elf-cpp
-LD		:= x86_64-unknown-elf-gcc -g
-OBJDUMP	:= x86_64-unknown-elf-objdump
+AS		:= x86_64-demos-elf-gcc
+AR		:= x86_64-demos-elf-ar
+CC		:= x86_64-demos-elf-gcc
+CPP		:= x86_64-demos-elf-cpp
+LD		:= x86_64-demos-elf-gcc -g
+OBJDUMP	:= x86_64-demos-elf-objdump
